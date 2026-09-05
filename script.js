af0d9c9405db59a6cfa9d109cd052aae353a8aeb
ahmed123tou <benhamoudahajer80@gmail.com>
2026-09-05 10:20:12 +0100

    Add Discord button click event handler

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+const discordButton = document.getElementById("discordButton");
 
+discordButton.addEventListener("click", () => {
+discordButton.disabled = true;
+
+```
+discordButton.textContent = "Connecting to Discord...";
+
+window.location.href = "/auth/discord";
+```
+
+});
