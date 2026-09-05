7384b6fba8d62ba95a1d7d2f5c3ab163363b4f1e
ahmed123tou <benhamoudahajer80@gmail.com>
2026-09-05 10:49:27 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 const discordButton = document.getElementById("discordButton");
 
 if (discordButton) {
-discordButton.addEventListener("click", () => {
+discordButton.addEventListener("click", function () {
 discordButton.disabled = true;
 discordButton.textContent = "Connecting to Discord...";
 
 
-    window.location.href =
-        "https://botzen-r4do.onrender.com/auth/discord";
+    const clientId = "1545716437987893308";
+    const redirectUri =
+        "https://botzen-r4do.onrender.com/callback";
+
+    const discordOAuth =
+        "https://discord.com/oauth2/authorize" +
+        "?client_id=" + encodeURIComponent(clientId) +
+        "&response_type=code" +
+        "&redirect_uri=" + encodeURIComponent(redirectUri) +
+        "&scope=identify";
+
+    window.location.href = discordOAuth;
 });
 
 
