ab06b7f3a7b4ee93389c1d5f772b416598be6b5f
ahmed123tou <benhamoudahajer80@gmail.com>
2026-09-05 10:34:22 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 const discordButton = document.getElementById("discordButton");
 
+if (discordButton) {
 discordButton.addEventListener("click", () => {
 discordButton.disabled = true;
-
-```
 discordButton.textContent = "Connecting to Discord...";
 
-window.location.href = "/auth/discord";
+```
+    window.location.href =
+        "https://botzen-r4do.onrender.com/auth/discord";
+});
 ```
 
-});
+}
